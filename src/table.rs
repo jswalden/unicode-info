2d5a73915979a5e10b97acde2d472da4fa3da2c7
Jeff Walden <jwalden@mit.edu>
2021-09-03 16:45:39 -0700

    Add more explanatory comments, change a couple names for better understandability.

--- src/table.rs
+++ src/table.rs
@@ -5,12 +5,14 @@
 use crate::types::NumericType;
 
 /// From a list `t` of integers, many of which will be equal in value, compute
-/// two separate lists `t1` and `t2`
+/// two separate lists `index1` and `index2` with given element types, plus the
+/// shift necessary to make the smallest possible two-level page table for `t`
+/// using them.
 pub struct TableSplit {
-    pub t1: Vec<u32>,
-    pub t1_elem_type: NumericType,
-    pub t2: Vec<u32>,
-    pub t2_elem_type: NumericType,
+    pub index1: Vec<u32>,
+    pub index1_elem_type: NumericType,
+    pub index2: Vec<u32>,
+    pub index2_elem_type: NumericType,
     pub shift: u32,
 }
 
@@ -74,9 +76,9 @@ fn test_get_element_type() {
 /// Print diagnostic information about the optimal table splitting.
 fn dump_best_split(s: &TableSplit, original_table: &Vec<u32>, bytes: usize) {
     eprintln!(
-        "Best: {t1_len}+{t2_len} bins at shift {shift}; {bytes} bytes",
-        t2_len = s.t2.len(),
-        t1_len = s.t1.len(),
+        "Best: {index1_len}+{index2_len} bins at shift {shift}; {bytes} bytes",
+        index1_len = s.index1.len(),
+        index2_len = s.index2.len(),
         shift = s.shift,
         bytes = bytes
     );
@@ -105,22 +107,52 @@ fn test_maximum_shift() {
     assert_eq!(compute_maximum_shift(&vec![0; 10]), 3);
 }
 
-/// Given a (large) table `t` of values, many of which are equal, split `t` into
-/// two tables and a `shift` such that
+/// Given a (large) table `t` of values, return the best possible splitting of
+/// that table into a two-level page table.
+///
+/// Various code point information is conceptually determined by looking up a
+/// value in an array, by code point.  Even given that we can limit this to only
+/// BMP code points, that's still a 64K-element array: a lot of memory!  And for
+/// our use cases, most of those elements have identical values.
+///
+/// The first possible optimization is to store
+///
+/// 1. A `table` array of every unique value in `t`.
+/// 1. An `index` array whose indexes are code points, whose elements are appropriate
+/// indexes into the first array.
+///
+/// This saves some memory, _if_ `t` elements occupy more memory than the
+/// elements of `index`.  But `index` is still just as long as `t`, so it
+/// doesn't save much.
+///
+/// The second optimization is to split that second array into two, giving us
+///
+/// 1. A `table` array of every unique value in `t`.
+/// 1. An `index1` array, indexed by the upper `(32 - N)` bits of a code point,
+/// that stores partial index values.
+/// 1. An `index2` array, indexed by an `index1` element shifted `N` bits upward
+/// plus the remaining lower `N` bits of a code point, whose elements are
+/// indexes into `table`.
+///
+/// Or to say it in pseudocode,
 ///
 /// ```text
 /// // assuming `i` is a valid index into table `t`
+/// let mask = (1 << N) - 1;
 /// for i in 0..t.len() {
-///     let mask = (1 << shift) - 1;
-///     let t1_entry = t1[i >> shift];
-///     let t1_index_component = t1_entry << shift;
+///     let index1_entry = index1[i >> N];
+///     let index1_index_component = index1_entry << N;
 ///     let mask_component = i & mask;
-///     assert!(t[i], t2[t1_index_component + mask_component]);
+///     assert!(t[i], index2[index1_index_component + mask_component]);
 /// }
 /// ```
+/// The idea is, we determine which `2**N`-element bucket size produces few
+/// enough unique elements in `table, and few enough elements (of small enough
+/// size) in `index1` and `index2`, to minimize space overall.
 ///
-/// where `shift` is chosen to minimize the memory consumed by `t1` and `t2`,
-/// assuming each is represented using the smallest possible integer type.
+/// (When `N = 0`, you can think of `index1` as an array of elements whose
+/// values are their indexes and `index2` as identical to `index` in the first
+/// optimization scheme.)
 pub fn split_table(t: &Vec<u32>) -> TableSplit {
     assert!(
         t.len() <= u32::MAX as usize,
@@ -128,52 +160,73 @@ pub fn split_table(t: &Vec<u32>) -> TableSplit {
     );
 
     // The memory consumed by the current best splitting.  (Initialized to all
-    // available memory so that the first iteration of the loop below will
-    // overwrite it.)
+    // available memory so that the first iteration of the loop below
+    // overwrites it.)
     let mut best_bytes = usize::MAX;
 
     // The current best splitting -- immediately overwritten.
     let mut best = TableSplit {
-        t1: vec![],
-        t1_elem_type: NumericType::U8,
-        t2: vec![],
-        t2_elem_type: NumericType::U8,
+        index1: vec![],
+        index1_elem_type: NumericType::U8,
+        index2: vec![],
+        index2_elem_type: NumericType::U8,
         shift: 0,
     };
 
+    // The maximum possible downshift of a valid index of `t` that will produce
+    // _some_ nonzero value.
     let max_shift = compute_maximum_shift(&t);
+
+    // For every possible shift that leaves some index into `t` nonzero...
     for candidate_shift in 0..=max_shift {
-        let mut t1 = vec![];
-        let mut t2 = vec![];
+        //  Let `t` be split into chunks of the corresponding size.
         let size = 1 << candidate_shift;
 
+        // Let `index1` and `index2` be empty arrays.
+        let mut index1 = vec![];
+        let mut index2 = vec![];
+
+        // Start a cache of chunks -> an index stored in `index1`.
         let mut bincache = std::collections::HashMap::<&[u32], u32>::new();
 
+        // For every chunk,
         for i in (0..t.len()).step_by(size) {
             let bin = &t[i..i + size];
 
             let index = match bincache.get(&bin) {
                 None => {
-                    let index = t2.len() as u32;
+                    // If the chunk isn't cached, append chunk to `index2`,
+                    // then use the chunk start as index.
+                    let index = index2.len() as u32;
                     bincache.insert(bin, index);
-                    t2.extend_from_slice(bin);
+                    index2.extend_from_slice(bin);
                     index
                 }
-                Some(index) => *index,
+                Some(index) => {
+                    // If the chunk's index is cached, use that index.
+                    *index
+                }
             };
-            t1.push(index >> candidate_shift);
+
+            // Add the index, shifted, to the end of `index1`.
+            index1.push(index >> candidate_shift);
         }
 
-        let t1_elem_type = get_element_type(&t1);
-        let t2_elem_type = get_element_type(&t2);
+        let index1_elem_type = get_element_type(&index1);
+        let index2_elem_type = get_element_type(&index2);
 
-        let bytes = get_size(t1_elem_type) * t1.len() + get_size(t2_elem_type) * t2.len();
+        let index1_size = get_size(index1_elem_type) * index1.len();
+        let index2_size = get_size(index2_elem_type) * index2.len();
+
+        // If the total size of `index1` and `index2` beats the previous best,
+        // update with the new best result.
+        let bytes = index1_size + index2_size;
         if bytes < best_bytes {
             best = TableSplit {
-                t1,
-                t1_elem_type,
-                t2,
-                t2_elem_type,
+                index1,
+                index1_elem_type,
+                index2,
+                index2_elem_type,
                 shift: candidate_shift,
             };
             best_bytes = bytes;
@@ -186,14 +239,17 @@ pub fn split_table(t: &Vec<u32>) -> TableSplit {
     {
         // Exhaustively verify that the decomposition is correct.
         let shift = best.shift;
-        let t1 = &best.t1;
-        let t2 = &best.t2;
+        let index1 = &best.index1;
+        let index2 = &best.index2;
         for i in 0..t.len() {
             let mask = (1 << shift) - 1;
-            let t1_entry = t1[i >> shift];
-            let t1_index_component = t1_entry << shift;
+            let index1_entry = index1[i >> shift];
+            let index1_index_component = index1_entry << shift;
             let mask_component = i & mask;
-            assert_eq!(t[i], t2[t1_index_component as usize + mask_component]);
+            assert_eq!(
+                t[i],
+                index2[index1_index_component as usize + mask_component]
+            );
         }
     }
 
